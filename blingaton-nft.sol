de807942efa35ecce0954fea9025ad1777575a6d
yoeri.de.schepper <yoeri.de.schepper@student.howest.be>
2021-10-26 16:57:15 +0200

    startup nft

---
+++ blingaton-nft.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+//https://gateway.pinata.cloud/ipfs/QmVebXP3jEgjPjyJB1dy8Rv3dejyCat4LZHq3jdENm7ELx/0000000000000000000000000000000000000000000000000000000000000001.json
+
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract BlingatonNFT is ERC1155,Ownable {
+    
+    constructor () ERC1155 ("https://gateway.pinata.cloud/ipfs/QmVebXP3jEgjPjyJB1dy8Rv3dejyCat4LZHq3jdENm7ELx/{id}.json" ){
+        
+    }
+    
+     //to test HashFunction use 0x6c00000000000000000000000000000000000000000000000000000000000000 for bytes32
+   
+    // inputs a string
+    // outputs a bytes32 unique hash
+    function HashFunction(string memory _password) public pure returns (bytes32 )  {
+        return keccak256(abi.encodePacked(_password));
+    }
+    
+    function mint (string memory _entryCode, bytes32) public onlyOwner  {}
+    
+    
+    
+}
